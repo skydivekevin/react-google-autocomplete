01829c1bfc2b8f1e03a3ca0dec373c3992252205
skydivekevin <skydivekevin@gmail.com>
2019-08-28 14:39:16 -0600

    changed cities to regions
    Signed-off-by: skydivekevin <skydivekevin@gmail.com>

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ export default class ReactGoogleAutocomplete extends React.Component {
 
   componentDidMount() {
     const {
-      // types = ['(cities)'],
+      types = ['(regions)'],
       componentRestrictions,
       bounds,
       fields = [
